a80e526b36cacbabeff72637052c06c3fe7838ed
Nooryaser2016 <nooor2016.yaser@gmail.com>
2026-02-18 18:09:57 +0400

    jii

--- mashipe.sql
+++ mashipe.sql
@@ -11,3 +11,19 @@ INSERT INTO Persons (PERSONID, lastname, address, city)
 VALUES (2, 'chris','next to north school','dubai');
 
 SELECT* FROM PERSONS WHERE PERSONID = 1;
+SELECT MIN(PERSONID)
+AS SMALLEST
+FROM Persons;
+SELECT * FROM Persons
+WHERE lastname LIKE 'a%';
+INSERT INTO Persons (PERSONID, lastname, address, city)
+VALUES (2, 'reem','rowhda, next to a fast food','dubai');
+INSERT INTO Persons (PERSONID, lastname, address, city)
+VALUES (2, 'jay','hamidiya 2','ajman');
+INSERT INTO Persons (PERSONID, lastname, address, city)
+VALUES (2, 'bella','next to hospital','sarjah');
+UPDATE Persons
+SET lastname = 'jack'
+WHERE city = 'dubai';
+SELECT * FROM Persons
+
